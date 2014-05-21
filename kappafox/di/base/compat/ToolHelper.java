ba6fb5c28a51c446a3119fd5877ca3cc57441592
kappafox <kappa.fox@gmail.com>
2014-05-22 01:00:20 +1000

    * New class to handle different tool interactions

---
+++ kappafox/di/base/compat/ToolHelper.java
@@ -0,0 +1,52 @@
+package kappafox.di.base.compat;
+
+import kappafox.di.DiscreteIndustry;
+import cpw.mods.fml.common.FMLCommonHandler;
+import ic2.api.item.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ToolHelper
+{
+
+	public static boolean isWrench(ItemStack item)
+	{
+		
+		ItemStack wrench = Items.getItem("wrench");
+		ItemStack ewrench = Items.getItem("electricWrench");
+
+		boolean ignore = false;
+		if(item != null)
+		{
+			
+			if(FMLCommonHandler.instance().getEffectiveSide().isServer() == true)
+			{
+				
+				boolean isGTWrench = false;
+				if(DiscreteIndustry.GTWrench != null)
+				{
+					if(item.getItem().getClass().getName().equalsIgnoreCase(DiscreteIndustry.GTWrench.getName()));
+					{
+						return true;
+					}
+				}
+				
+				if(DiscreteIndustry.BCWrench != null)
+				{
+					Item i = item.getItem();
+					if(DiscreteIndustry.BCWrench.isAssignableFrom(i.getClass()))
+					{
+						//System.out.println("yo!");
+						return true;
+					}
+				}
+			
+				if(item.getDisplayName().equalsIgnoreCase(wrench.getDisplayName()) || item.getDisplayName().equalsIgnoreCase(ewrench.getDisplayName()))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}

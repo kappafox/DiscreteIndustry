d4254e0d57b25fbf8a3b2c646bc0cd19edab5936
kappafox <kappa.fox@gmail.com>
2014-05-24 01:17:13 +1000

    * Updated Version Number

--- kappafox/di/DiscreteIndustry.java
+++ kappafox/di/DiscreteIndustry.java
@@ -38,7 +38,7 @@ public class DiscreteIndustry
 	//Globals
 	public static final String MODID = "DiscreteIndustry";
 	public static final String NAME = "Discrete Industry";
-	public static final String VERSION = "1.0.1e";
+	public static final String VERSION = "1.0.1f";
 	public static final String DEPENDENCIES = "required-after:IC2";
 	
 	
